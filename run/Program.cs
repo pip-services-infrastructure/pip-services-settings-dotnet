176d5171f8725e536943c58a9586908146a57c52
KateNegrienko <katya@negrienko.com>
2018-01-29 21:49:32 +0200

    init

---
+++ run/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using PipServices.Settings.Container;
+
+namespace run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var task = (new SettingsProcess()).RunAsync(args);
+                task.Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+        }
+    }
+}
